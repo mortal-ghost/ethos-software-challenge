927477a221965f216b14d29b3c69eeb3126a8e99
mortal-ghost <anmoldeepsingh148@gmail.com>
2022-12-23 15:51:28 +0530

    Added MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
