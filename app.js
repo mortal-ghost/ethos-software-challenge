e73037f1323125f3a6b07e42cc27941ffdaad711
mortal-ghost <anmoldeepsingh148@gmail.com>
2023-01-05 18:54:58 +0530

    Added React

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-const { v4: uuidV4 } = require('uuid');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const LocalStrategy = require('passport-local');
-const passportLocalMongoose = require('passport-local-mongoose');
 const session = require('express-session');
 const url = "mongodb://localhost:27017/ethos_project";
 const userRoute = require('./routes/user');
@@ -15,7 +13,6 @@ const fs = require('fs');
 const http = require('http');
 const socketIO = require('socket.io');
 const fileupload = require("express-fileupload");
-var path = require('path');
 const server = http.Server(app);
 var multer = require('multer');
 var upload = multer({ dest: 'public/videos' });
@@ -46,32 +43,29 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use(flash());
-const io =socketIO(server);
+const io = socketIO(server);
 
-
-io.on('connection',socket =>{
+io.on('connection', socket => {
     console.log('Someone connected');
     let AUDIO_ID;
-    socket.on('hello',async(value)=>{
+    socket.on('hello', async (value) => {
         socket.join(value);
         AUDIO_ID = value;
         console.log(value);
-        let tempAudio = await Audio.findOne({id:AUDIO_ID});
-        socket.emit('comments',tempAudio.comments);
+        let tempAudio = await Audio.findOne({ id: AUDIO_ID });
+        socket.emit('comments', tempAudio.comments);
 
     });
 
-    socket.on('addcomment',async(comment)=>{
+    socket.on('addcomment', async (comment) => {
         console.log(comment);
-        let tempAudio = await Audio.findOne({id:AUDIO_ID});
+        let tempAudio = await Audio.findOne({ id: AUDIO_ID });
         tempAudio.comments.push(comment)
-        socket.emit('comments',tempAudio.comments);
+        socket.emit('comments', tempAudio.comments);
         await tempAudio.save();
 
-    })
-
-
-})
+    });
+});
 //When you even redirect from any route, it will come below first, I mean the request will re propagate.
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
@@ -82,29 +76,32 @@ app.use((req, res, next) => {
         CurrentUserID = null;
     }
     next();
-})
+});
+
 app.use('/user', userRoute);
+
 app.get('/', (req, res) => {
 
     res.render('index.ejs');
 });
+
 app.post('/url', async (req, res) => {
 
     console.log(req.body);
     const newAudio = Audio({});
     newAudio.name = req.body.name;
     await newAudio.save();
-    newAudio.path =__dirname + '/public/audio/' +  req.user.username + '_' + newAudio._id + '.mp3';
+    newAudio.path = __dirname + '/public/audio/' + req.user.username + '_' + newAudio._id + '.mp3';
     newAudio.comments = [];
     newAudio.id = String(newAudio._id);
     newAudio.userid = String(req.user._id);
     await newAudio.save();
     const filename = req.user.username + '_' + newAudio._id + '.mp3';
-    let command = "python3 " + "getyoutubeaudio.py " + "'" + req.body.url + "'" + " " + (req.user.username + '_' + newAudio._id );
+    let command = "python3 " + "getyoutubeaudio.py " + "'" + req.body.url + "'" + " " + (req.user.username + '_' + newAudio._id);
     console.log(command);
 
     let end = 0;
-    exec(command, async(error, stdout, stderr) => {
+    exec(command, async (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
@@ -113,69 +110,69 @@ app.post('/url', async (req, res) => {
             console.log(`stderr: ${stderr}`);
             return;
         }
-        
+
         console.log(`stdout: ${stdout}`);
         executefunc();
     });
 
-    let value =0;
+    let value = 0;
 
-    const executefunc = ()=>{
-        if(value==0){
+    const executefunc = () => {
+        if (value == 0) {
             console.log('The below is value');
             console.log(value);
-            res.redirect('/play_music/'+newAudio._id);
+            res.redirect('/play_music/' + newAudio._id);
             value++;
         }
     }
-})
-app.post('/files',upload.single('file'),(req,res)=>{
+});
+
+app.post('/files', upload.single('file'), (req, res) => {
     console.log(req.files);
-    if(req.files){
+    if (req.files) {
         console.log(req.files);
         var file = req.files.file;
         var filename = file.name
         console.log(filename);
-        file.mv('./public/videos/' + filename, function(err){
-            if(err){
+        file.mv('./public/videos/' + filename, function (err) {
+            if (err) {
                 res.send(err);
             }
-            else{
+            else {
                 res.send("file uploaded");
             }
         })
     }
-})
-app.get('/play_music/:id',async (req,res)=>{
+});
+
+app.get('/play_music/:id', async (req, res) => {
     console.log('I am called');
-    if(req.user){
+    if (req.user) {
         console.log(req.params.id);
-        const newAudio = await Audio.findOne({id:req.params.id});
+        const newAudio = await Audio.findOne({ id: req.params.id });
         console.log('Audio output below!');
         console.log(newAudio);
         let filename = req.user.username + '_' + req.params.id + '.mp3';
-        res.render('play.ejs',{ filename: filename,comments: newAudio.comments,audio_id: req.params.id,Audio: Audio});
+        res.render('play.ejs', { filename: filename, comments: newAudio.comments, audio_id: req.params.id, Audio: Audio });
     }
-    else{
+    else {
         res.render('index.ejs');
     }
 
-})
+});
 
 
-app.get('/all_projects', async(req,res)=>{
+app.get('/all_projects', async (req, res) => {
 
-    const allAudios = await Audio.find({userid: String(req.user._id)});
+    const allAudios = await Audio.find({ userid: String(req.user._id) });
 
-    res.render('allprojects.ejs',{allAudios: allAudios});
-})
+    res.render('allprojects.ejs', { allAudios: allAudios });
+});
+
 server.listen(3000, (err) => {
-
     if (err) {
         console.log(err);
-    }
-    else {
+    } else {
         console.log('App is listening in port 3000');
     }
-
-})
+});
---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './index.css';
+
+function App() {
+  return (
+    <div className="text-4xl">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
