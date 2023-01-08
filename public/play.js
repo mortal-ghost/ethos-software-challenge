d3a243e54ee2cd5dea0de6eaa27c12bde4a64287
saiakshitharthi <asaiakshith03@gmail.com>
2023-01-08 18:17:08 +0530

    removed shit photo

--- public/play.js
+++ public/play.js
@@ -21,8 +21,7 @@ const createComment = (comment) => {
 
     <div class="flex  items-center w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
 
-  <a href="#" class="flex items-center mt-6 mb-6 mr-6"><img src="https://avatars.githubusercontent.com/u/71964085?v=4" alt="avatar" class="hidden object-cover w-14 h-14 mx-4 rounded-full sm:block">
-  </a>
+  
 
     <div><h3 class="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">${comment.title}</h3>
       <p class="text-sm font-bold text-gray-300">${timeStamp}</p>
